d9e66279b2d4ea8857d1da8ceb1b9d0c27ea0623
ravipandey <ravipan31.1991@gmail.com>
2020-04-26 16:33:12 +0530

    added config client

---
+++ springConfigClient/src/test/java/com/test/springConfigClient/SpringConfigClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.springConfigClient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringConfigClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
